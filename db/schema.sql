f265f6c22885799a80c767175df90b32b33ca802
mac2 <mauricio.trevinon91@gmail.com>
2023-05-16 18:28:38 -0600

    Fixed functionality

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,30 @@
-DROP DATABASE IF EXISTS business_manager_db;
-CREATE DATABASE business_manager_db;
+CREATE DATABASE employee_db;
 
-USE business_manager_db;
+USE employee_db;
 
-CREATE TABLE department (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
--- server.js
+++ server.js
@@ -1,156 +1,243 @@
-const fs = require('fs');
+const express = require('express');
 const inquirer = require('inquirer');
-const htmlGenerator = require('./src/htmlGenerator');
-const teamMembers = [];
-const manager = require('./library/manager');
-const engineer = require('./library/engineer');
-const intern = require('./library/intern');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+const { CallTracker } = require('assert');
+require('dotenv').config()
 
-const generateManager = async () => {
-  const questions = [
-    {
-      type: 'input',
-      name: 'name',
-      message: "What is the manager's name?",
-      validate: (input) => input !== '' || 'Please enter the team manager name',
-    },
-    {
-      type: 'input',
-      name: 'id',
-      message: "What is the manager's employee ID?",
-      validate: (input) =>
-        input !== '' || 'Please enter the team manager employee ID',
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: "What is the manager's email address?",
-      validate: (input) =>
-        input !== '' || 'Please enter the team manager email address',
-    },
-    {
-      type: 'input',
-      name: 'officeNumber',
-      message: "What is the manager's office number?",
-      validate: (input) =>
-        input !== '' || 'Please enter the team manager office number',
-    },
-  ];
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'W@lker!',
+    database: 'tracker_db'
+});
 
-  const managerInfo = await inquirer.prompt(questions);
-  const { name, id, email, officeNumber } = managerInfo;
-  const newManager = new manager(name, id, email, officeNumber);
-  teamMembers.push(newManager);
-  console.log(newManager);
-  addEmployee();
+connection.connect(err => {
+    if (err) console.log(err);
+    else InquirerPrompt();
+});
+
+const InquirerPrompt = async () => {
+    try {
+        const { choices } = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'choices',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add department',
+                    'Add role',
+                    'Add employee',
+                    'Update all departments',
+                    'Update employee infomation',
+                    'Exit'
+                ]
+            }
+        ]);
+
+        switch (choices) {
+            case "View all departments":
+                await showDepartments();
+                break;
+            case "View all roles":
+                await showRoles();
+                break;
+            case "View all employees":
+                await showEmployees();
+                break;
+            case "Add department":
+                await addDepartments();
+                break;
+            case "Add role":
+                await addRoles();
+                break;
+            case "Add employee":
+                await addEmployees();
+                break;
+            case "Update all departments":
+                await allDepartments();
+                break;
+            case "Update employee infomation":
+                await employeeInfomation();
+                break;
+            case "Exit":
+                connection.end();
+                break;
+            default:
+                console.log("Invalid choice");
+                break;
+        }
+    } catch (err) {
+        console.log(err);
+    }
 };
 
-const generateEngineer = async () => {
-  const questions = [
-    {
-      type: 'input',
-      name: 'name',
-      message: "What is the engineer's name?",
-      validate: (input) =>
-        input !== '' || 'Please enter the team engineer name',
-    },
-    {
-      type: 'input',
-      name: 'id',
-      message: "What is the engineer's employee ID?",
-      validate: (input) =>
-        input !== '' || 'Please enter the team engineer employee ID',
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: "What is the engineer's email address?",
-      validate: (input) =>
-        input !== '' || 'Please enter the team engineer email address',
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: "What is the engineer's GitHub user name?",
-      validate: (input) =>
-        input !== '' || 'Please enter the engineer GitHub user',
-    },
-  ];
-
-  const engineerInfo = await inquirer.prompt(questions);
-  const { name, id, email, github } = engineerInfo;
-  const newengineer = new engineer(name, id, email, github);
-  teamMembers.push(newengineer);
-  console.log(newengineer);
-  addEmployee();
+const showDepartments = async () => {
+    try {
+        console.log('All departments are showing.');
+        const mysql = `SELECT department.id AS id, department.name AS department FROM department`;
+        const [rows] = await connection.promise().query(mysql);
+        console.table(rows);
+        InquirerPrompt();
+    } catch (err) {
+        console.log(err);
+    }
 };
 
-const generateIntern = async () => {
-  const questions = [
-    {
-      type: 'input',
-      name: 'name',
-      message: "What is the intern's name?",
-      validate: (input) => input !== '' || 'Please enter the team intern name',
-    },
-    {
-      type: 'input',
-      name: 'id',
-      message: "What is the intern's employee ID?",
-      validate: (input) =>
-        input !== '' || 'Please enter the team intern employee ID',
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: "What is the intern's email address?",
-      validate: (input) =>
-        input !== '' || 'Please enter the team intern email address',
-    },
-    {
-      type: 'input',
-      name: 'school',
-      message: "What is the intern'school name?",
-      validate: (input) =>
-        input !== '' || 'Please enter the intern school name',
-    },
-  ];
-
-  const internInfo = await inquirer.prompt(questions);
-  const { name, id, email, school } = internInfo;
-  const newintern = new intern(name, id, email, school);
-  teamMembers.push(newintern);
-  console.log(newintern);
-  addEmployee();
+const showRoles = async () => {
+    try {
+        console.log('Show all roles.');
+        const mysql = `SELECT roles.id, roles.title, department.name AS department FROM roles LEFT JOIN department ON roles.department_id = department.id`;
+        const [rows] = await connection.promise().query(mysql);
+        console.table(rows);
+        InquirerPrompt();
+    } catch (err) {
+        console.log(err);
+    }
 };
 
-generateManager();
-
-async function addEmployee() {
-  const employeeTypes = {
-    manager: generateManager,
-    Engineer: generateEngineer,
-    Intern: generateIntern,
-    None: () =>
-      fs.writeFileSync(
-        './public/teamOverview.html',
-        htmlGenerator(teamMembers)
-      ),
-  };
-
-  const { employeeType } = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'employeeType',
-      message: 'Do you want to add another team member?',
-      choices: Object.keys(employeeTypes),
-    },
-  ]);
-
-  const generateEmployee = employeeTypes[employeeType];
-  if (generateEmployee) {
-    await generateEmployee();
-  } else {
-    console.log('Invalid input');
+const addRoles = async () => {
+  try {
+      const { roles, salary } = await inquirer.prompt([
+          {
+              type: 'input',
+              name: 'roles',
+              message: "What do you want to add?",
+          },
+          {
+              type: 'input',
+              name: 'salary',
+              message: 'What is your yearly salary?',
+          }
+      ]);
+      const parameters = [roles, salary];
+      const roles_var = `SELECT name, id FROM department`;
+      const [data] = await connection.promise().query(roles_var);
+      const departments = data.map(({ name, id }) => ({ name: name, value: id }));
+      const { department_id } = await inquirer.prompt([
+          {
+              type: 'list',
+              name: 'department_id',
+              message: "What department is this role in?",
+              choices: departments
+          }
+      ]);
+      parameters.push(department_id);
+      const mysql = `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`;
+      await connection.promise().query(mysql, parameters);
+      console.log(`Added ${roles} to roles`);
+      showRoles();
+  } catch (err) {
+      console.log(err);
   }
-}
+};
+
+
+const showEmployees = async () => {
+    try {
+        console.log('All employees are showing.');
+        const mysql = `SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS department, roles.salary, CONCAT(mgr.first_name, mgr.last_name) AS manager FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employee mgr ON employee.manager_id = mgr.id`;
+        const [rows] = await connection.promise().query(mysql);
+        console.table(rows);
+        InquirerPrompt();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const updateEmployee = async () => {
+    try {
+        const employeemysql = `SELECT * FROM employee`;
+        const [data] = await connection.promise().query(employeemysql);
+        const employees = data.map(({ id, first_name, last_name }) => ({ name: first_name + " " + last_name, value: id }));
+        const { name } = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'name',
+                message: 'Which employee do we want to update?',
+                choices: employees
+            }
+        ]);
+        const parameters = [];
+        parameters.push(name);
+        const role_var = `SELECT * FROM role`;
+        const [dataRoles] = await connection.promise().query(role_var);
+        const roles = dataRoles.map(({ id, title }) => ({ name: title, value: id }));
+        const { role } = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'role',
+                message: 'What is the new role?',
+                choices: roles
+            }
+        ]);
+        parameters.push(role);
+        let employee = parameters[0]
+        parameters[0] = role
+        parameters[1] = employee
+        const mysql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+        await connection.promise().query(mysql, parameters);
+        console.log('Role has been updated.');
+        showEmployees();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const addDepartments = async () => {
+    try {
+        const { department } = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'department',
+                message: 'What department do you want to add?',
+            }
+        ]);
+        const mysql = `INSERT INTO department (name) VALUES (?)`;
+        await connection.promise().query(mysql, department);
+        console.log(`Added ${department} to departments`);
+        showDepartments();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const addEmployees = async () => {
+    try {
+        const { first_name, last_name } = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: 'Your First Name?',
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: 'Your Last Name?',
+            }
+        ]);
+        const parameters = [first_name, last_name];
+        const roles_var = `SELECT roles.id, roles.title FROM roles`;
+        const [data] = await connection.promise().query(roles_var);
+        const roles = data.map(({ id, title }) => ({ name: title, value: id }));
+        const { role } = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'role',
+                message: 'What is your role?',
+                choices: roles
+            }
+        ]);
+        parameters.push(role);
+        const mysql = `INSERT INTO employee (first_name, last_name, role_id) VALUES (?,?,?)`;
+        await connection.promise().query(mysql, parameters);
+        console.log(`Added ${first_name} ${last_name} to employees`);
+        showEmployees();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
